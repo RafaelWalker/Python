69b32b041f24284b0b59c0cc296095bee4322ffe
Rafael Walker Batista <rafaelwalkerbatista@gmail.com>
2017-07-07 21:54:39 -0300

    Doblo, Triplo, Raiz

---
+++ Exercicios/Exe6.py
@@ -0,0 +1,5 @@
+import math
+numero = int(input('Informe um número: '))
+print('O Dobro de {} é: {}'.format(numero, (numero*2)))
+print('O Triplo de {} é: {}'.format(numero, (numero*3)))
+print('A Raiz Quadrada de {} é: {}'.format(numero, (math.sqrt(numero))))
