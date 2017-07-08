7c739ef1b8a83b0942e5f970428b57f95dd55791
Rafael Walker Batista <rafaelwalkerbatista@gmail.com>
2017-07-08 17:39:22 -0300

    Média Aritmética

---
+++ Exercicios/Exe7.py
@@ -0,0 +1,3 @@
+nota1 = float(input('Primeira nota do Aluno: '))
+nota2 = float(input('Segunda nota do Aluno: '))
+print('A média entre {:.1f} e {:.1f} é igual a: {:.1f}'.format(nota1, nota2, ((nota1 + nota2)/2)))
