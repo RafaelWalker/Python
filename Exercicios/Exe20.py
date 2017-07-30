8dd1e21004b840d3c2b718e0417ef49564a9bb43
Rafael Walker Batista <rafaelwalkerbatista@gmail.com>
2017-07-30 16:01:56 -0300

    Sorteio Aluno Random

---
+++ Exercicios/Exe20.py
@@ -0,0 +1,9 @@
+import random
+print('Informe os Nomes dos 4 Alunos a Sortear')
+aluno1 = input('Primeiro Aluno: ')
+aluno2 = input('Segundo Aluno: ')
+aluno3 = input('Terceiro Aluno: ')
+aluno4 = input('Quarto Aluno: ')
+lista = [aluno1, aluno2, aluno3, aluno4]
+sorteado = random.choice(lista)
+print('O Aluno Escolhido foi: ', sorteado)
