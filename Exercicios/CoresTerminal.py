8cdd9f2bcad45a1b76087798d683191632e36dc7
Rafael Walker Batista <rafaelwalkerbatista@gmail.com>
2017-08-19 18:31:22 -0300

    Cores terminal 2

--- Exercicios/CoresTerminal.py
+++ Exercicios/CoresTerminal.py
@@ -1,2 +1,16 @@
 # primeiro parametro colchetes tipo de fonte, segundo cor da fonte e terceiro com fundo
 print('\033[1;31;43mOlá Mundo!\033[m')
+
+#segundo modo
+nome = 'Rafael'
+print('Olá! Segundo teste de cor {}{}{}'.format('\033[4;34m', nome, '\033[m'))
+
+#terceiro modo
+cores = {'limpa':'\033[m',
+         'azul':'\033[34m',
+         'amarelo':'\033[33m',
+         'pretoebranco':'\033[7;30m'}
+
+print('-----------------------------------------------------------')
+
+print('Olá! Muito prazer em te conhecer, {}{}{}'.format(cores['amarelo'], nome, cores['limpa']))
